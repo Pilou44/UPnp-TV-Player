3359a5156ecb9b95f9c02a3db1cd3c9921796068
Pilou44 <guillaume@wechantloup.com>
2020-08-04 18:38:30 +0200

    First commit

---
+++ app/src/main/java/com/wechantloup/upnpvideoplayer/rootSetter/RootSetterActivity.kt
@@ -0,0 +1,205 @@
+package com.wechantloup.upnpvideoplayer.rootSetter
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wechantloup.upnpvideoplayer.R
+import com.wechantloup.upnpvideoplayer.dataholder.DlnaElement
+import org.fourthline.cling.android.AndroidUpnpService
+import org.fourthline.cling.android.AndroidUpnpServiceImpl
+import org.fourthline.cling.model.action.ActionInvocation
+import org.fourthline.cling.model.message.UpnpResponse
+import org.fourthline.cling.model.meta.Device
+import org.fourthline.cling.model.meta.LocalDevice
+import org.fourthline.cling.model.meta.RemoteDevice
+import org.fourthline.cling.model.meta.RemoteService
+import org.fourthline.cling.registry.DefaultRegistryListener
+import org.fourthline.cling.registry.Registry
+import org.fourthline.cling.support.contentdirectory.callback.Browse
+import org.fourthline.cling.support.model.BrowseFlag
+import org.fourthline.cling.support.model.DIDLContent
+
+class RootSetterActivity : Activity() {
+
+    private var selectedElement: DlnaElement? = null
+    private lateinit var list: RecyclerView
+    private lateinit var adapter: RootSetterAdapter
+    private var bound: Boolean = false
+    val mHandler = Handler()
+    private var mUpnpService: AndroidUpnpService? = null
+    private val mRegistryListener = BrowseRegistryListener(mHandler, ::addDevice)
+    private val mAllFiles = mutableListOf<DlnaElement>()
+
+    private val mServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            Log.i(TAG, "Service connected")
+            val upnpService = (service as AndroidUpnpService)
+                .also { mUpnpService = it }
+
+            // Get ready for future device advertisements
+            upnpService.registry.addListener(mRegistryListener)
+
+            // Now add all devices to the list we already know about
+            for (device in upnpService.registry.devices) {
+                mRegistryListener.newDeviceAdded(device)
+            }
+
+            // Search asynchronously for all devices, they will respond soon
+            upnpService.controlPoint.search()
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            Log.i(TAG, "Service disconnected")
+            mUpnpService = null
+        }
+
+        override fun onBindingDied(name: ComponentName?) {
+            Log.i(TAG, "Service onBindingDied")
+        }
+
+        override fun onNullBinding(name: ComponentName?) {
+            Log.i(TAG, "Service onNullBinding")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root_setter)
+
+        list = findViewById(R.id.list)
+        adapter = RootSetterAdapter(mAllFiles, ::onItemClick)
+        list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        list.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // This will start the UPnP service if it wasn't already started
+        Log.i(TAG, "Start UPnP Service")
+        applicationContext.bindService(
+            Intent(this, AndroidUpnpServiceImpl::class.java),
+            mServiceConnection,
+            Context.BIND_AUTO_CREATE
+        )
+        bound = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mUpnpService?.registry?.removeListener(mRegistryListener)
+        // This will stop the UPnP service if nobody else is bound to it
+        if (bound) {
+            applicationContext.unbindService(mServiceConnection)
+        }
+    }
+
+    private fun onItemClick(element: DlnaElement) {
+        val position = mAllFiles.indexOf(element)
+        if (!element.isExpanded) {
+            mUpnpService!!.controlPoint
+                .execute(object : Browse(element.service, element.path, BrowseFlag.DIRECT_CHILDREN) {
+                    override fun received(
+                        arg0: ActionInvocation<*>?,
+                        didl: DIDLContent
+                    ) {
+                        mHandler.post {
+                            val itemsQuantity = didl.containers.size
+                            Log.i(TAG, "found $itemsQuantity items.")
+                            for (i in didl.containers.indices) {
+                                val newElement = DlnaElement(
+                                    didl.containers[i].title,
+                                    didl.containers[i].id,
+                                    element
+                                )
+                                mAllFiles.add(position + i + 1, newElement)
+                            }
+                            adapter.notifyItemRangeInserted(position + 1, itemsQuantity)
+                        }
+                    }
+
+                    override fun updateStatus(status: Status) {}
+                    override fun failure(
+                        arg0: ActionInvocation<*>?,
+                        arg1: UpnpResponse,
+                        arg2: String
+                    ) {
+                    }
+                })
+            element.isExpanded = true
+        }
+        selectedElement = element
+        adapter.setSelectedElement(position)
+    }
+
+    private fun addDevice(device: Device<*,*,*>) {
+        if (device.isFullyHydrated) {
+            for (service in device.services) {
+                if (service.serviceType.type == "ContentDirectory") {
+                    val d = DlnaElement(
+                        device,
+                        service as RemoteService
+                    )
+                    var position: Int = mAllFiles.indexOf(d)
+                    if (position >= 0) {
+                        Log.i(TAG, "Replace device")
+                        // Device already in the list, re-set new value at same position
+                        mAllFiles.remove(d)
+                        mAllFiles.add(position, d)
+                        adapter.notifyItemChanged(position)
+                    } else {
+                        Log.i(TAG, "Add new device")
+                        mAllFiles.add(d)
+                        position = mAllFiles.indexOf(d)
+                        adapter.notifyItemInserted(position)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = RootSetterActivity::class.java.simpleName
+    }
+
+    internal class BrowseRegistryListener(private val handler: Handler, private var function: (Device<*, *, *>) -> Unit) : DefaultRegistryListener() {
+
+        /* Discovery performance optimization for very slow Android devices! */
+        override fun remoteDeviceDiscoveryStarted(registry: Registry?, device: RemoteDevice) {
+            newDeviceAdded(device)
+        }
+
+        override fun remoteDeviceDiscoveryFailed(
+            registry: Registry?,
+            device: RemoteDevice?,
+            ex: Exception?
+        ) {
+        }
+
+        /* End of optimization, you can remove the whole block if your Android handset is fast (>= 600 Mhz) */
+        override fun remoteDeviceAdded(registry: Registry?, device: RemoteDevice) {
+            newDeviceAdded(device)
+        }
+
+        override fun remoteDeviceRemoved(registry: Registry?, device: RemoteDevice?) {}
+        override fun localDeviceAdded(registry: Registry?, device: LocalDevice) {
+            newDeviceAdded(device)
+        }
+
+        override fun localDeviceRemoved(registry: Registry?, device: LocalDevice?) {}
+
+        fun newDeviceAdded(device: Device<*,*,*>) {
+            handler.post {
+                function(device)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/wechantloup/upnpvideoplayer/rootSetter/RootSetterAdapter.kt
@@ -0,0 +1,78 @@
+package com.wechantloup.upnpvideoplayer.rootSetter
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.wechantloup.upnpvideoplayer.R
+import com.wechantloup.upnpvideoplayer.dataholder.DlnaElement
+
+class RootSetterAdapter(
+    private var list: List<DlnaElement>,
+    private var onItemClicked: (DlnaElement) -> Unit
+): RecyclerView.Adapter<RootSetterAdapter.DlnaElementHolder>() {
+
+    private var selectedElement = -1
+
+    private var defaultBackgroundColor = Color.parseColor("#00ffffff")
+    private var selectedBackgroundColor = Color.parseColor("#66666666")
+    private var focusedBackgroundColor = Color.parseColor("#66ffe680")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DlnaElementHolder {
+        val view = parent.inflate(R.layout.item_dlna_element)
+        view.isFocusable = true
+        view.isFocusableInTouchMode = true
+        view.setOnFocusChangeListener { v: View, hasFocus: Boolean ->
+            when {
+                hasFocus -> v.setBackgroundColor(focusedBackgroundColor)
+                v.tag == selectedElement -> v.setBackgroundColor(selectedBackgroundColor)
+                else -> v.setBackgroundColor(defaultBackgroundColor)
+            }
+        }
+        return DlnaElementHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: DlnaElementHolder, position: Int) {
+        val element = list[position]
+        holder.apply {
+            itemView.tag = position
+            name.text = element.name
+            itemView.setOnClickListener { onItemClicked(element) }
+            val padding: Int = holder.layout.paddingRight
+            layout.setPadding(
+                padding * (element.indent + 1),
+                padding, padding, padding
+            )
+            if (position == selectedElement) {
+                itemView.setBackgroundColor(selectedBackgroundColor)
+            } else {
+                itemView.setBackgroundColor(defaultBackgroundColor)
+            }
+        }
+    }
+
+    class DlnaElementHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.name)
+        val layout: RelativeLayout = view.findViewById(R.id.layout)
+    }
+
+    private fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View =
+        LayoutInflater
+            .from(context)
+            .inflate(layoutRes, this, attachToRoot)
+
+    fun setSelectedElement(position: Int) {
+        val oldSelection = selectedElement
+        selectedElement = position
+        notifyItemChanged(oldSelection)
+        notifyItemChanged(selectedElement)
+    }
+}
